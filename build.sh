16a28fe57f6059242837595f354e1b58ddcdb194
Nick Sarkauskas <nsarka00@gmail.com>
2025-09-30 20:29:50 -0400

    Update deploy scripts

--- build.sh
+++ build.sh
@@ -6,5 +6,6 @@ cd nsarka
 hugo
 cd ..
 mv nsarka/public ./public
+echo Public folder created, deploy it to master with ./publish.sh
 
 set +x
--- publish.sh
+++ publish.sh
@@ -2,6 +2,14 @@
 
 set -x
 
+# Readd CNAME
+touch ./public/CNAME
+echo "nsarka.com" > ./public/CNAME
+
+# Check out the master branch for pushing the public folder
 git checkout master
+
+cp -r ./public/* ./
+rm -rf ./public
 
 set +x
